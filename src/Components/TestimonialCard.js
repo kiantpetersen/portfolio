0c65a6ad6cea07d2dd681dc9ee69fd875b496daa
unknown <kiantpetersen@gmail.com>
2023-03-22 14:42:09 +0200

    fixing testimonial cover

--- src/Components/TestimonialCard.js
+++ src/Components/TestimonialCard.js
@@ -9,7 +9,6 @@ function TestimonialCard(props) {
                 <img className='user-img' src={props.img} alt='user_img' />
             </div>
             <div className='testimonial-textbox'>
-                <div className='cover'></div>
                 <h3 className='testimonial-heading'>Amazing modern deseigns</h3>
                 <p className='text-content'>Lorem ipsum dolor sit amet, condimentum ac. Curabitur libero leo, commodo ultrices augue ut, placerat luctus mauris. Cras accumsan molestie neque, ac ultricies elit venenatis nec. Vivamus viverra condimentum nulla, et dictum massa feugiat nec. Cras eget felis ut justo facilisis ornare.</p>
                 <p className='testimonial-author'> &sim;  Mark Dent</p>
