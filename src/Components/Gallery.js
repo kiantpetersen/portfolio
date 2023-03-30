ad310a0e91262a7100f6589fc8f997aa5d0e8937
unknown <kiantpetersen@gmail.com>
2023-03-30 21:49:08 +0200

    Testimonial card redsigned

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/css/compCss/gallery.css'
 import '../styles/css/queries.css'
 import galleryImg1 from '../styles/images/gallery-img-1.jpg'
@@ -15,21 +15,47 @@ import galleryImg11 from '../styles/images/gallery-img-12.jpg'
 import galleryImg12 from '../styles/images/gallery-img-13.jpg'
 
 function Gallery(props) {
+    const [image, setImage] = useState(galleryImg2)
     let imagesArr = [galleryImg1, galleryImg2, galleryImg3, galleryImg4, galleryImg5, galleryImg6, galleryImg7, galleryImg8, galleryImg9, galleryImg10, galleryImg11, galleryImg12]
+
+
+    function setOverlayImg(e) {
+        let overlay = document.querySelector('#overlay')
+        let img = document.querySelector('#img-container')
+        img.scrollIntoView({ behavior: 'smooth' })
+        overlay.classList.toggle('hidden')
+        console.log(e.target.classList)
+
+        setImage(e.target.src)
+    }
 
     function getImages() {
         return imagesArr.map((image, key) => {
             return (
-                <img className='gallery-img' key={key} alt='image_yep' src={image} />
+                <img onClick={setOverlayImg} className='gallery-img' key={key} alt='image_yep' src={image} />
             )
         })
     }
+    function setVisibility(e) {
+        e.preventDefault()
+        e.target.classList.toggle('hidden')
+
+    }
+
 
     return (
         <div className='gallery-section'>
             <h1 className='primary-heading gallery-heading'>Gallery</h1>
             <div className='gallery'>
                 {getImages()}
+            </div>
+            <div onClick={setVisibility} id='overlay' className='overlay hidden'>
+
+                <div id='img-container' className='over-img-container'>
+                    <img className='overlay-img' src={image} alt='full-img' />
+
+                </div>
+
             </div>
         </div>
     );
--- src/Components/TestimonialCard.js
+++ src/Components/TestimonialCard.js
@@ -9,7 +9,7 @@ function TestimonialCard(props) {
                 <img className='user-img' src={props.img} alt='user_img' />
             </div>
             <div className='testimonial-textbox'>
-                <h3 className='testimonial-heading'>Amazing modern deseigns</h3>
+                <h3 className='testimonial-heading'>Amazing modern designs</h3>
                 <p className='text-content'>Lorem ipsum dolor sit amet, condimentum ac. Curabitur libero leo, commodo ultrices augue ut, placerat luctus mauris. Cras accumsan molestie neque, ac ultricies elit venenatis nec. Vivamus viverra condimentum nulla, et dictum massa feugiat nec. Cras eget felis ut justo facilisis ornare.</p>
                 <p className='testimonial-author'> &sim;  Mark Dent</p>
             </div>
